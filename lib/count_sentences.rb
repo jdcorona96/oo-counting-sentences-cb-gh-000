c44dc854a5ef684e735742a6a973fa6263d170fa
Joseph Corona <josed.hc96@gmail.com>
2018-11-18 03:38:39 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    ret = self.split(/[.?!]+\s*/)
+    #puts ret
+    ret.count
   end
 end
